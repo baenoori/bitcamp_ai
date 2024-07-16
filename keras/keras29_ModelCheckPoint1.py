d09811473da5cf082cede939f40f23e34fe5c5c6
baenoori <nooribee123@gmail.com>
2024-07-16 15:05:45 +0900

    0726

--- keras/keras29_ModelCheckPoint1.py
+++ keras/keras29_ModelCheckPoint1.py
@@ -1,3 +1,67 @@
+# 28_1 copy
+
+import sklearn as sk
+from sklearn.datasets import load_boston   
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+import time
+
+#1. 데이터 
+dataset = load_boston()
+print(dataset)
+print(dataset.DESCR) 
+print(dataset.feature_names)
+
+
+x = dataset.data    # x데이터 분리
+y = dataset.target  # y데이터 분리, sklearn 문법
+
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=231)
+
+####### scaling (데이터 전처리) #######
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+scaler = MinMaxScaler()
+scaler.fit(x_train)
+x_train = scaler.transform(x_train)
+x_test = scaler.transform(x_test)
+
+#2. 모델 구성
+model = Sequential()
+model.add(Dense(32, input_shape=(13,)))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(16, activation='relu'))
+model.add(Dense(16, activation='relu'))
+model.add(Dense(1))
+
+#3. 컴파일, 훈련
+model.compile(loss='mse', optimizer='adam')
+
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+es = EarlyStopping(monitor='val_loss', mode='min', 
+    
+)
+
+start = time.time()
+hist = model.fit(x_train, y_train, epochs=10, batch_size=16,
+          verbose=3, 
+          validation_split=0.1,
+          )
+end = time.time()
+
+#4. 평가, 예측
+loss = model.evaluate(x_test, y_test, verbose=1)    # 추가
+print('loss :', loss)
+
+y_predict = model.predict(x_test)
+
+r2 = r2_score(y_test, y_predict)
+print('r2 score :', r2)
+
+print("걸린 시간 :", round(end-start,2),'초')
 
 
 
