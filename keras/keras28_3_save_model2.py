1f04c3223388d2760acc05ee5a0b43aced311f1e
baenoori <nooribee123@gmail.com>
2024-07-26 14:44:14 +0900

    0726 1444

---
+++ keras/keras28_3_save_model2.py
@@ -0,0 +1,142 @@
+# 28_1 copy
+
+import sklearn as sk
+print(sk.__version__)   # 0.24.2
+from sklearn.datasets import load_boston   
+
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+import time
+
+#1. 데이터 
+dataset = load_boston()
+print(dataset)
+print(dataset.DESCR) 
+print(dataset.feature_names)
+# ['CRIM' 'ZN' 'INDUS' 'CHAS' 'NOX' 'RM' 'AGE' 'DIS' 'RAD' 'TAX' 'PTRATIO'
+#  'B' 'LSTAT']
+
+x = dataset.data    # x데이터 분리
+y = dataset.target  # y데이터 분리, sklearn 문법
+
+print(x)
+print(x.shape)  # (506, 13)
+print(y)
+print(y.shape)  # (506, )
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=231)
+
+####### scaling (데이터 전처리) #######
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.preprocessing import MaxAbsScaler, RobustScaler
+# scaler = MinMaxScaler()
+# scaler = StandardScaler()
+# scaler = MaxAbsScaler()
+scaler = RobustScaler()
+scaler.fit(x_train)
+x_train = scaler.transform(x_train)
+x_test = scaler.transform(x_test)
+
+print(x_train)
+print(np.min(x_train), np.max(x_train))     # 0.0  1.0
+print(x_test)
+print(np.min(x_test), np.max(x_test))     # -0.028269883151149644 1.0974124809741248
+
+
+#2. 모델 구성
+model = Sequential()
+# model.add(Dense(32, input_dim=13))
+model.add(Dense(10, input_shape=(13,)))      # 이미지 : input_shape=(8,8,1)
+model.add(Dense(5))
+model.add(Dense(1))
+
+model.summary()
+
+# model.save("./_save/keras28/keras28_1_save_model.h5")
+
+
+#3. 컴파일, 훈련
+model.compile(loss='mse', optimizer='adam')
+start = time.time()
+hist = model.fit(x_train, y_train, epochs=10, batch_size=16,
+          verbose=3, 
+          validation_split=0.1
+          )
+end = time.time()
+
+model.save("./_save/keras28/keras28_3_save_model.h5")
+
+
+#4. 평가, 예측
+loss = model.evaluate(x_test, y_test, verbose=1)    # 추가
+print('loss :', loss)
+
+y_predict = model.predict(x_test)
+
+r2 = r2_score(y_test, y_predict)
+print('r2 score :', r2)
+
+print("걸린 시간 :", round(end-start,2),'초')
+
+# print("=================== hist ==================")
+# print(hist)
+
+# print("================ hist.history =============")
+# print(hist.history)
+
+# print("================ loss =============")
+# print(hist.history['loss'])
+# print("================ val_loss =============")
+# print(hist.history['val_loss'])
+# print("==================================================")
+
+
+# import matplotlib.pyplot as plt
+
+# plt.rcParams['font.family'] ='Malgun Gothic'     # 한글 깨짐 해결, 폰트 적용
+
+# plt.figure(figsize=(9,6))   # 9 x 6 사이즈 
+# plt.plot(hist.history['loss'],c='red', label='loss',)  # y값 만 넣으면 시간 순으로 그려줌 
+# plt.plot(hist.history['val_loss'], c='blue', label = 'val_loss')
+# plt.legend(loc='upper right')   # 우측 상단 label 표시
+# plt.title('보스턴 Loss')
+# plt.xlabel('epoch')
+# plt.ylabel('loss')
+# plt.grid()  # 격자 표시
+# plt.show()
+
+
+""" 
+epochs=1000
+batch_size=16
+loss : 21.718276977539062
+r2 score : 0.7094395307207769
+걸린 시간 : 17.65 초
+
+[scaling - minmaxscaler]
+loss : 18.30896759033203
+r2 score : 0.7550514000724255
+걸린 시간 : 18.31 초
+
+[scaling - standardscaler]
+loss : 19.139081954956055
+r2 score : 0.7439455775297008
+걸린 시간 : 17.89 초
+
+[scaling - MaxAbsScaler]
+loss : 19.072237014770508
+r2 score : 0.7448398598162087
+걸린 시간 : 18.05 초
+
+[scaling - RobustScaler]
+loss : 19.063827514648438
+r2 score : 0.7449524156637057
+걸린 시간 : 18.48 초
+
+"""
+
+
+
